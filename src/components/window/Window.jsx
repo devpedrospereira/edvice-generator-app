92caca9ea567ba5eb4ced5742145757af6db50b3
devpedrospereira <pedros.pereira558@gmail.com>
2022-03-02 17:35:51 -0300

    implementação do component Window

---
+++ src/components/window/Window.jsx
@@ -0,0 +1,9 @@
+import styles from "./style.module.css"
+
+export default function Windows(){
+    return(
+        <div className={styles.container}>
+                test
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Windows from '../components/window/Window'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Windows/>
     </div>
   )
 }
