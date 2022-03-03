af5c53a5f633bc03763286325db01ab70a8dae06
devpedrospereira <pedros.pereira558@gmail.com>
2022-03-02 23:08:17 -0300

    estilização, implemetação do component Window e rederização dos advices

--- src/components/window/Window.jsx
+++ src/components/window/Window.jsx
@@ -1,9 +1,42 @@
-import styles from "./style.module.css"
+import { useEffect, useState } from "react"
+import styles from "./style.module.scss"
 
 export default function Windows(){
+
+    const [advices, setadvice] = useState({})
+    const [clic, setclic] = useState(Boolean)
+    
+    useEffect(() => {
+        fetch("https://api.adviceslip.com/advice")
+            .then(response => response.json())
+            .then(data => setadvice(data))
+    },[clic])
+
+        const render = () => {       
+            if(advices.slip){
+                const {id,advice} = advices.slip;
+                return(
+                    <>
+                        <div className={styles.advice}>
+                            <h2>Adivice #{id}</h2>
+                            <h1>{advice}</h1>
+                        </div>
+                        
+                        <div className={styles.division}>
+                            <img src="./accets/pattern-divider-desktop.svg" alt="division" />
+                        </div>
+                        
+                        <button onClick={()=> setclic(!clic)} className={styles.button}>
+                            <img src="./accets/icon-dice.svg" alt="button" />
+                        </button>
+                    </>
+                )
+            }
+    }
+
     return(
         <div className={styles.container}>
-                test
+                {render()}
         </div>
     )
 }
