37bf069a3f0074e6ab677732851938274a31cbc7
devpedrospereira <pedros.pereira558@gmail.com>
2022-03-03 01:11:06 -0300

    correções na estilização

--- src/components/window/Window.jsx
+++ src/components/window/Window.jsx
@@ -1,49 +1,53 @@
-import { useEffect, useState } from "react"
-import styles from "./style.module.scss"
+/* eslint-disable @next/next/no-img-element */
+import { useEffect, useState } from "react";
+import styles from "./style.module.scss";
 
-export default function Windows(){
-
-    const [advices, setadvice] = useState({})
-    const [clic, setclic] = useState(false)
-    
-    
-    const active = () =>{
-        // alert("aasd")
-        // console.log("asdaad")
-    }
+export default function Windows() {
+    const [advices, setAdvice] = useState({});
+    const [clic, setclic] = useState(false);
 
     useEffect(() => {
         fetch("https://api.adviceslip.com/advice")
-            .then(response => response.json())
-            .then(data => setadvice(data))
-    },[clic])
+            .then((response) => response.json())
+            .then((data) => setAdvice(data));
+    }, [clic]);
+    // requisitando os dados da api, e os armazenando na constante data
+    const render = () => {
+        if (advices.slip) {
+            const { id, advice } = advices.slip;
+            return (
+                <>
+                    <div className={styles.advice}>
+                        <h2>Adivice #{id}</h2>
+                        <h1>{advice}</h1>
+                    </div>
 
-        const render = () => {       
-            if(advices.slip){
-                const {id,advice} = advices.slip;
-                return(
-                    <>
-                        <div className={styles.advice}>
-                            <h2>Adivice #{id}</h2>
-                            <h1>{advice}</h1>
-                        </div>
-                        
-                        <div className={styles.division}>
-                            <img src="./accets/pattern-divider-desktop.svg" alt="division" />
-                        </div>
-                        
-                        <button  onClick={()=> setclic(!clic)} className={styles.button}
->
-                            <img className={(clic === true) ? styles.buttonTrue: styles.buttonFalse} src="./accets/icon-dice.svg" alt="button" />
-                        </button>
-                    </>
-                )
-            }
-    }
+                    <div className={styles.division}>
+                        <img
+                            src="./accets/pattern-divider-desktop.svg"
+                            alt="division"
+                        />
+                    </div>
+
+                    <button
+                        onClick={() => setclic(!clic)}
+                        className={styles.button}
+                    >
+                        <img
+                            className={clic === true ? styles.buttonTrue : styles.buttonFalse}
+                            src="./accets/icon-dice.svg"
+                            alt="button"
+                        />
+                        {/* a condição ternaria acima, chama a classe para rotacionar o botão ao ser clicado */}
+                    </button>
+                </>
+            );
+        }
+    };
 
     return(
         <div className={styles.container}>
-                {render()}
+            {render()}
         </div>
     )
 }
